fdae40ee9ea0587667f3b5a8fb8093a8dfce6262
Alex <sashka.greha@gmail.com>
2021-01-20 16:29:10 +0200

    init

---
+++ statistics.js
@@ -0,0 +1,23 @@
+function createStatistics() {
+    let counter = 0;
+    const listener = () => counter ++;
+    let isDestroyed = false;
+
+    document.addEventListener("click", listener);
+
+    return {
+        destroy() {
+            document.removeEventListener('click', listener);
+            isDestroyed = true;
+        },
+
+        getClicks() {
+            if (isDestroyed) return 'Statistics is Destroyed';
+
+
+
+            return counter;
+        }
+    }
+}
+window.statistics = createStatistics();
